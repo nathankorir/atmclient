071622e0b9c724848f2f0558337cdde5304768e2
nathankorir <nathankipron@gmail.com>
2021-03-03 11:42:11 +0300

    auth client initial commit

---
+++ src/main/java/com/atmclient/atmclient/AtmclientApplication.java
@@ -0,0 +1,13 @@
+package com.atmclient.atmclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atmclient/atmclient/AtmclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmclient.atmclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
